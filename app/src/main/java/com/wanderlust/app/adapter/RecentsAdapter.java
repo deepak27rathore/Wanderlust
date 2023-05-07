05f353e95f4eef139c337d09b00a52fee702e7d0
deepak27rathore <deepak2003rathore@gmail.com>
2023-05-07 23:59:12 +0530

    recents_row_item added

--- app/src/main/java/com/wanderlust/app/adapter/RecentsAdapter.java
+++ app/src/main/java/com/wanderlust/app/adapter/RecentsAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,21 +15,21 @@ import com.wanderlust.app.model.RecentsData;
 
 import java.util.List;
 
-public class RecentsAdapter extends RecyclerView.Adapter<>{
+public class RecentsAdapter extends RecyclerView.Adapter<RecentsAdapter.RecentsViewHolder>{
 
     Context context;
     List<RecentsData> recentsDataList;
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(context).inflate(R.layout.,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.recents_row_item,parent,false);
         return new RecentsViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecentsViewHolder holder, int position) {
 
     }
 
@@ -39,8 +41,17 @@ public class RecentsAdapter extends RecyclerView.Adapter<>{
 
     public static final class RecentsViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView placeImage;
+        TextView placeName, countryName, price;
+
         public RecentsViewHolder(@NonNull View itemView){
             super(itemView);
+
+            placeImage = itemView.findViewById(R.id.place_image);
+            placeName = itemView.findViewById(R.id.place_name);
+            countryName = itemView.findViewById(R.id.country_name);
+            price = itemView.findViewById(R.id.price);
+
         }
     }
 }
