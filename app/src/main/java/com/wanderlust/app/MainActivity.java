5b6e98b1929591373f9ff82ba9e5196c6d32fd3e
Ishan Rathore <ishaanrathore04@gmail.com>
2023-05-16 22:11:09 +0530

    Done.

--- app/src/main/java/com/wanderlust/app/MainActivity.java
+++ app/src/main/java/com/wanderlust/app/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         topPlacesDataList.add(new TopPlacesData("Manali", "India", "From Rs.2600", R.drawable.topplaces));
         topPlacesDataList.add(new TopPlacesData("Victoria Mahal", "India", "From Rs.1600", R.drawable.topplaces ));
         topPlacesDataList.add(new TopPlacesData("Leh-Ladakh", "India", "From Rs.1600", R.drawable.topplaces ));
-        topPlacesDataList.add(new TopPlacesData("Ooty", "India", "From Rs.1600", R.drawable.topplaces ));
+        topPlacesDataList.add(new TopPlacesData("Ooty", "India", "From Rs.2600", R.drawable.topplaces ));
 
         setTopPlacesRecycler((topPlacesDataList));
 
